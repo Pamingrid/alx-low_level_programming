c2602218898498da6c874eefbbcc366f3d46f970
Pamingrid <pamingrid26@gmail.com>
2022-06-30 15:14:53 -0700

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -13,7 +13,7 @@ long l = 59;
 long fd = 1;
 long syscall = 1;
 long ret = 0;
-__asm__ ("syscall": "=a" (ret) : "a" (syscall), "D" (fd), "S" (s), "d" (l));
+__asm__ ("syscall" : "=a" (ret) : "a" (syscall), "D" (fd), "S" (s), "d" (l));
 
 	return (1);
 }
