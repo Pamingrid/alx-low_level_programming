99bdc0ece941f9ea5510b7eb2ef0e698bd6c2cbe
Pamingrid <pamingrid26@gmail.com>
2022-06-30 14:39:34 -0700

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -13,12 +13,12 @@ int main(void)
 	long fd = 1;
 	long syscall = 1;
 	long ret = 0;
-	__asm__ ("syscall"
+	__asm__ ("syscall")
 
 			: "=a" (ret)
 			: "a" (syscall),
 			"D" (fd),
 			"S" (s),
-			"d" (l));
+			"d" (l);
 	return (1);
 }
