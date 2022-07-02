10ab2578ac122372eacaf912cf750e0410b7c337
Pamingrid <pamingrid26@gmail.com>
2022-07-02 13:10:36 -0700

    11. The success combination in business is: Do what you do better... and: do more of what you do...

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -38,10 +38,10 @@ if (c != d && d != e && e < d && d < c)
 putchar('0' + e);
 	putchar('0' + d);
 	putchar('0' + c);
-	if (c + d + e != 17)
+	if (c + d + e != 9 + 8 + 7)
 
 	{
-		putchar('0');
+		putchar(',');
 		putchar(' ');
 	}
 }
