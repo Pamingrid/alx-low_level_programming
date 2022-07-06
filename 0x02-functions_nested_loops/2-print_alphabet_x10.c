fe98c73a277214c1e72aab785b13ec338a329081
Pamingrid <pamingrid26@gmail.com>
2022-07-06 13:16:47 -0700

    push

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,18 +1,19 @@
 #include "main.h"
+
 /**
  * main - print alphabet
  *
  * Return: 0
  */
 
-void print_alphabet_x10(void)
+void print_alphabet_x10(void);
 {
 char i;
 int j;
 
-for (j = 0; j < 10; j++)
-{
 for (i = 'a'; i <= 'z'; i++)
+{
+for (j = 0; j < 10; j++)
 _putchar(i);
 }
 _putchar('\n');
