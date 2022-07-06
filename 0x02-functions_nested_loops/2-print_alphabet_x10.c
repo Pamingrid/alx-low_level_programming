e2dbf1fefe1f940a3736d419581905137733e8c0
Pamingrid <pamingrid26@gmail.com>
2022-07-06 13:49:56 -0700

    push

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * main - print alphabet
+ * print_alphabet_x10 - print 10 times the alphabet in lowercase
+ * Description: print 10 times in lowercase
  *
  * Return: 0
  */
@@ -10,7 +11,7 @@ void print_alphabet_x10(void)
 {
 	char c = '\0';
 	int n = 10;
-while (--n)
+while (n--)
 {
 	for (c = 'a'; c <= 'z'; c++)
 		_putchar(c);
