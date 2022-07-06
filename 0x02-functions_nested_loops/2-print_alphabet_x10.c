bbc6ef47d792f1ef788dee4cb2fc53fbb5044697
Pamingrid <pamingrid26@gmail.com>
2022-07-06 11:51:26 -0700

    print alphabet x 10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - print alphabet
+ *
+ * Return: 0
+ */
+void print_alphabet_x10(void);
+{
+	char i;
+	int j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 'a'; i < = 'z'; i++)
+			_putchar(i);
+		_putchar('_n');
+	}
+}
