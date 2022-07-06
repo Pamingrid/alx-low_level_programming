c512126b337ec47c74c83b3e64e747c4f8e04a12
Pamingrid <pamingrid26@gmail.com>
2022-07-06 14:03:27 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _islower - check if character is lower case
+ * @c: character to be checked
+ *
+ * Return: 1 if lowercase 0 otherwise
+ */
+int _islower(int c)
+{
+	return ('a' <= c && c <= 'z' ? 1 : 0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,5 +5,6 @@ int _putint(int j);
 void print_putchar(void);
 void print_alphabet(void);
 void print_alphabet_x10(void);
+int _islower(int c);
 
 #endif /*MAIN_H */
