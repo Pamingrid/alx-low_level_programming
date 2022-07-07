a173ef14c89acf14071c24cc6c9496410512139e
Pamingrid <pamingrid26@gmail.com>
2022-07-06 18:31:00 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - check main
+ * @a: An integer a
+ * @b: An integer b
+ * Description: function that adds two integers and returns the result
+ * Return: Result of addition of a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
