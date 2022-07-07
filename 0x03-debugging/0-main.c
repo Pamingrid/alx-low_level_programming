6e3494878e1e81c07e5e45db8390db5d6496ea98
Pamingrid <pamingrid26@gmail.com>
2022-07-07 06:49:00 -0700

    update

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,18 @@
 #include "main.h"
 /**
  * main - test the function negative or positive
- * Description the variable n will return a value
+ * Description: the variable i will return a value
  * Return: 0
  */
 int main(void)
 {
 	int i;
 
-	i = 0;
+srand(time(0));
+
+i = rand() - RAND_MAX / 2;
+
+i = 98;
 
 	positive_or_negative(i);
 
