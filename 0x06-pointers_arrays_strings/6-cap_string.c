92d09d7d2f88cff74078f876b52832d628460cf5
Pamingrid <pamingrid26@gmail.com>
2022-07-14 08:56:21 -0700

    update

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,29 +1,37 @@
 #include "main.h"
 /**
- * cap_string - function that capitalizes all words of a string
- * @s: input parameter
- * Return: return the pointer to dest
+ * cap_string - capitalizes all words of a string
+ * @str: the string to change the first letter of a word in uppercase
+ *
+ * Return: capitalizes letters
  */
-char *cap_string(char *s)
+char *cap_string(char *str)
 {
-	int count = 0, i;
-int separators[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+	int i;
+	int j;
+	char c[] = {44, 59, 46, 33, 63, 34, 40, 41, 123, 125, 32, 10, 9};
 
-if (*(s + count) >= 97 && *(s + count) <= 122)
-	*(s + count) = *(s + count) - 32;
-count++;
-while (*(s + count) != '\0')
-{
-	for (i = 0; i < 13; i++)
+	i = 0;
+
+	while (str[i] != '\0')
 	{
-		if (*(s + count) == separators[i])
+		if (i == 0 && str[i] >= 97 && str[i] <= 122)
 		{
-		if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
-			*(s + (count + 1)) = *(s + (count + 1)) - 32;
-			break;
+			str[i] = str[i] - 32;
 		}
+		j = 0;
+
+		while (c[j] != '\0')
+		{
+		if (c[j] == str[i] && (str[i + 1] >= 97 && str[i + 1] <= 122))
+		{
+			str[i + 1] = str[i + 1] - 32;
+		}
+		j++;
+
+		}
+		i++;
+
 	}
-	count++;
-}
-return (s);
+	return (str);
 }
