7189be2bfe91ed5e794c85cbd71fefa2ffb320c8
Pamingrid <pamingrid26@gmail.com>
2022-07-23 23:36:12 -0700

    I got it

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
  * _puts - print a string followed by a new line
- * @s: string to print
+ * @str: string to print
  * Return: Nothing
  */
-void _puts(char *s)
+void _puts(char *str)
 {
 	int i;
 
-	for (i = 0; *(s + i) != 0; i++)
+	for (i = 0; *(str + i) != 0; i++)
 	{
-		_putchar(s[i]);
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 }
