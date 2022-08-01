d132d27b5fc457fa29bcbd42dda51061f15bd6aa
Pamingrid <pamingrid26@gmail.com>
2022-08-01 07:52:13 -0700

    update first commit

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -4,15 +4,16 @@
 /**
  * init_dog - Initializes a variable of type struct dog
  * @d: dog object
- * @name: dog age
+ * @name: dog name
+ * @age: dog age
  * @owner: Owner of dog
  *
- * Return: Always 0.
+ * Return: Nothing
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
-		d = malloc(sizeof(struct dog));
+		return;
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,16 +7,15 @@
  * @owner: Owner of dog
  * Description: This is a structure which defines a new type of dog
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
-typedef struct dog dog_t;
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *name);
+dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 char *_strncpy(char *dest, char *src, int n);
 int _strlen(char *s);
